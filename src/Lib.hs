e381a241442b83affc7409f09f6a6fa5f2aa23df
xvblack <xvblue@gmail.com>
2017-02-09 22:34:05 -0800

    first working version

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,7 +18,7 @@ class (Show a) => Message a where
   serialize :: a -> B.ByteString
   deserialize :: B.ByteString -> a 
 
-data AnyMessage = forall a. Message a => AnyMessage a
+data AnyMessage = forall a. Message a => AnyMessage {unMessage :: a}
 instance Show AnyMessage where
   show (AnyMessage m) = show m
 
@@ -40,17 +40,17 @@ send :: (Message m) => a -> m -> NetM a ()
 send addr msg = F.Free $ Send addr (AnyMessage msg) $ F.Pure ()
 receive :: (Message m) => NetM a (Maybe (a, m))
 receive = F.Free $ Receive $ F.Pure . (fmap (\(a, b) -> (a, deserialize b))) 
-wait :: NetM a ()
-wait = F.Free $ WaitForMessage $ F.Pure ()
 getPeers :: forall a . NetM a [a]
 getPeers = F.Free (GetPeers F.Pure)
+getSelf :: NetM a a
+getSelf = F.Free $ GetSelf F.Pure
 
 program = do
   peers <- getPeers
+  self <- getSelf
   forM peers $ \peer -> do
-    send peer "A"
+    send peer $ "A" ++ show self
   forever $ do
-    wait
     receive >>= \m -> case m of
       Nothing -> return ()
       Just (peer, msg) -> do
@@ -66,11 +66,12 @@ prettyM _ = return ()
 runRepeatly :: a -> (a -> IO a) -> IO ()
 runRepeatly a f = do
   r <- f a
-  runRepeatly f r
+  runRepeatly r f
 
 stepNumPerLoop = 10
 
-data EmulatorState = EmulatorState {messages :: M.Map Int (S.Seq B.ByteString)} 
+data EmulatorState = EmulatorState {messages :: M.Map Int (S.Seq (Int, B.ByteString))} 
+  deriving (Show)
 
 emptyEmulatorState addrs = EmulatorState {
   messages = M.fromList $ zip addrs $ repeat S.empty
@@ -80,20 +81,38 @@ emulateM :: Int -> NetM Int () -> IO ()
 emulateM peerNum program = do
   let addrs = [1..peerNum]
   state <- newMVar $ emptyEmulatorState addrs
-  forever $ do
-    forM addrs $ \addr ->
-      let peers = filter (!= addr) addrs in 
-        emulateIndividualMachine state addr peers stepNumPerLoop program
+  runRepeatly (repeat program) $ \continuations -> do
+    newContinuations <- emulateRoundM state addrs continuations
+    currState <- readMVar state
+    putStrLn $ show currState
+    return newContinuations
+
+emulateRoundM :: (MVar EmulatorState) -> [Int] -> [NetM Int ()] -> IO [NetM Int ()]
+emulateRoundM state addrs programs = 
+  forM (zip addrs programs) $ \(addr, program) ->
+    let peers = filter (/= addr) addrs in 
+      emulateIndividualMachine state addr peers stepNumPerLoop program
 
 emulateIndividualMachine :: (MVar EmulatorState) -> Int -> [Int] -> Int -> NetM Int () -> IO (NetM Int ())
 emulateIndividualMachine state addr peers stepNum program
   | stepNum == 0 = return program
   | otherwise = case program of
-      F.Free (Send addr msg next) -> do
-        modifyMVar_ state $ \state -> return $ state {messages = M.adjust (S.|> (serialize msg)) addr $ messages state}
+      F.Free (Send destination (AnyMessage msg) next) -> do
+        modifyMVar_ state $ \state -> return $ state {messages = M.adjust (S.|> (addr, serialize $ msg)) destination $ messages state}
         return next
+      F.Free (Receive callback) -> do
+        head <- modifyMVar state $ \state -> case M.lookup addr $ messages state of
+          Just seq -> case S.viewl seq of
+            head S.:< remains -> return $ (state {messages = M.adjust (const remains) addr $ messages state}, Just head)
+            S.EmptyL -> return (state, Nothing)
+          Nothing -> return (state, Nothing)
+        return $ callback head
+      F.Free (GetPeers callback) -> do
+        return $ callback peers
+      F.Free (GetSelf callback) -> do
+        return $ callback addr
       _ -> return $ return ()
     
 
 someFunc :: IO ()
-someFunc = prettyM program
+someFunc = emulateM 10 program
